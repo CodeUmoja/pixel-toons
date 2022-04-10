f3b473be1288dafbe356a1851569c196dad41d8c
svitbar <svitbar1102@gmail.com>
2022-04-10 23:36:31 +0300

    change name of the function

--- color.js
+++ color.js
@@ -12,7 +12,7 @@ export class Color {
   }
 
   //"Factory method" that creates color from given HEX value
-  static fromHex(hexColor) {
+  static hexToRGB(hexColor) {
     const r = parseInt(hexColor.slice(1, 3), 16);
     const g = parseInt(hexColor.slice(3, 5), 16);
     const b = parseInt(hexColor.slice(5, 7), 16);
