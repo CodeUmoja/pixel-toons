079903e4b13b445f53b01800c1a7b78355b64706
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-04-18 21:18:52 +0200

    implemented zoom
    Co-authored-by: MytsV <70377928+MytsV@users.noreply.github.com>

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,6 @@
 import { renderCanvas } from './canvas_renderer.js';
 import { Canvas } from './canvas.js';
+import { zoom } from './zoom.js';
 
 const canvasWidth = 40;
 const canvasHeight = 40;
@@ -7,3 +8,17 @@ const canvasHeight = 40;
 window.onload = () => {
   renderCanvas(new Canvas({ width: canvasWidth, height: canvasHeight }));
 };
+
+const zoomCodes = {
+  '+': true,
+  '=': true,
+  '-': false,
+  '_': false
+};
+
+document.addEventListener('keypress', (event) => {
+  const index = Object.keys(zoomCodes).indexOf(event.key);
+  if (index !== -1) {
+    zoom(zoomCodes[event.key]);
+  }
+});
